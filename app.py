f6b76063b6f6f8dc56fda77401f44cc9217fdec6
Adarsh-890 <adarshtiwari8912@gmail.com>
2025-02-19 21:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  # Ye root route define karega
 def home():
-    return "Hello, Flask is working!"
+    return "Flask is working!"
 
 if __name__ == '__main__':
     app.run(debug=True)
