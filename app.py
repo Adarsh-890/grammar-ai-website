bb16c55bc9ae553f8d772b9adb9b9701ad0ba798
Adarsh-890 <adarshtiwari8912@gmail.com>
2025-02-19 13:56:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
 import requests
 
 LANGUAGE_TOOL_API = "https://api.languagetool.org/v2/check"
